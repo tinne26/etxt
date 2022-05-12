63dc5a17550ffccf572092653e2de1eb90152105
tinne26 <tinne26k@gmail.com>
2022-05-12 10:24:10 +0200

    Updating hello_world example to match the README.

--- examples/ebiten/hello_world/main.go
+++ examples/ebiten/hello_world/main.go
@@ -1,11 +1,10 @@
 package main
 
-import "image/color"
-import "time"
 import "log"
+import "time"
+import "image/color"
 
 import "github.com/hajimehoshi/ebiten/v2"
-
 import "github.com/tinne26/etxt"
 
 // NOTICE: this is the example from the readme, but it's not
@@ -17,11 +16,12 @@ type Game struct { txtRenderer *etxt.Renderer }
 func (self *Game) Layout(int, int) (int, int) { return 400, 400 }
 func (self *Game) Update() error { return nil }
 func (self *Game) Draw(screen *ebiten.Image) {
-	millis := time.Now().UnixMilli() // don't do this in actual games ;)
-	blue := (millis/16)%512
+	millis := time.Now().UnixMilli() // (you should usually avoid using time)
+	blue := (millis/16) % 512
 	if blue >= 256 { blue = 511 - blue }
 	changingColor := color.RGBA{ 0, 255, uint8(blue), 255 }
 
+	// set relevant text renderer properties and draw
 	self.txtRenderer.SetTarget(screen)
 	self.txtRenderer.SetColor(changingColor)
 	self.txtRenderer.Draw("Hello World!", 200, 200)
@@ -36,20 +36,19 @@ func main() {
 	}
 
 	// check that we have the fonts we want
-	// (we are not using this many fonts in the example, though...)
-	// (showing it for completeness, you don't need this in most cases)
+	// (shown for completeness, you don't need this in most cases)
 	if !fontLib.HasFont("League Gothic Regular") { log.Fatal("missing font 1") }
 	if !fontLib.HasFont("Carter One"           ) { log.Fatal("missing font 2") }
 	if !fontLib.HasFont("Roboto Bold"          ) { log.Fatal("missing font 3") }
 
 	// check that the fonts have the characters we want
-	// (showing it for completeness, you don't need this in most cases)
+	// (shown for completeness, you don't need this in most cases)
 	err = fontLib.EachFont(checkMissingRunes)
 	if err != nil { log.Fatal(err) }
 
 	// create a new text renderer and configure it
-	txtRenderer    := etxt.NewStdRenderer()
-	glyphsCache    := etxt.NewDefaultCache(10*1024*1024) // 10MB
+	txtRenderer := etxt.NewStdRenderer()
+	glyphsCache := etxt.NewDefaultCache(10*1024*1024) // 10MB
 	txtRenderer.SetCacheHandler(glyphsCache.NewHandler())
 	txtRenderer.SetFont(fontLib.GetFont("League Gothic Regular"))
 	txtRenderer.SetAlign(etxt.YCenter, etxt.XCenter)
@@ -58,16 +57,16 @@ func main() {
 	// run the "game"
 	ebiten.SetWindowSize(400, 400)
 	err = ebiten.RunGame(&Game{ txtRenderer })
-	if err != nil {
-		log.Fatalf("ebiten.RunGame error: %s", err.Error())
-	}
+	if err != nil { log.Fatal(err) }
 }
 
-// helper used after loading fonts
+// helper function used with FontLibrary.EachFont to make sure
+// all loaded fonts contain the characters / alphabet we want
 func checkMissingRunes(name string, font *etxt.Font) error {
-	const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 .,;:!?-()[]"
+	const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	const symbols = "0123456789 .,;:!?-()[]{}_&#@"
 
-	missing, err := etxt.GetMissingRunes(font, alphabet)
+	missing, err := etxt.GetMissingRunes(font, letters + symbols)
 	if err != nil { return err }
 	if len(missing) > 0 {
 		log.Fatalf("Font '%s' missing runes: %s", name, string(missing))
