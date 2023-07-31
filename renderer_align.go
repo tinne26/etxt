0a5c140fb765bbda48a8abe30833852d1f447738
tinne26 <tinne26k@gmail.com>
2023-07-31 18:22:13 +0200

    Will I ever figure out align names?

--- renderer_align.go
+++ renderer_align.go
@@ -1,47 +1,67 @@
 package etxt
 
-// Aligns are used to define how to interpret the coordinates passed
-// to renderer operations. For example, if you try to draw text at
-// (0, 0) with any align that's not top-left, the result is going to
-// be clipped or not visible at all.
+// Aligns tell a [Renderer] how to interpret the coordinates
+// that some of its methods —like [Renderer.Draw]()— receive.
+//
+// More concretely: given some text, we have a text box or bounding
+// rectangle that contains it. The text align specifies which part of
+// that bounding box has to be aligned to the given coordinates.
+// 
+// For example: drawing "POOP" at (0, 0) with a centered align
+// means that the center of the text box will be aligned to the
+// (0, 0) coordinate. We should see the bottom half of "OP" on
+// the top-left corner of our screen.
 //
 // See [Renderer.SetAlign]() for more details.
 type Align uint8
 
 // Returns the vertical component of the align. If the
 // align is valid, the result can only be one of the
-// following: [Top], [TopBaseline], [YCenter], [BottomBaseline],
-// [Bottom].
+// following: [Top], [Midline], [VertCenter], [Baseline],
+// [LastMidline], [LastBaseline], [Bottom].
 func (self Align) Vert() Align { return alignVertBits & self }
 
 // Returns the horizontal component of the align. If the
 // align is valid, the result can only be one of the
-// following: [Left], [XCenter], [Right].
+// following: [Left], [HorzCenter], [Right].
 func (self Align) Horz() Align { return alignHorzBits & self }
 
-// Align constants for renderer operations. Vertical
-// and horizontal aligns can be combined with a bitwise
-// OR (|).
+// Align constants for renderer operations.
+//
+// Since aligns have both a vertical and a horizontal component,
+// you can use a bitwise OR when setting them, e.g.,
+// [Renderer.SetAlign](etxt.Left | etxt.Bottom). To retrieve or
+// compare the individual components, avoid bitwise operations
+// and use [Align.Vert]() and [Align.Horz]() instead.
 const (
-	Left       Align = 0b0010_0000 // horizontal align
-	HorzCenter Align = 0b0100_0000 // horizontal align
-	Right      Align = 0b1000_0000 // horizontal align
+	// Horizontal aligns
+	Left       Align = 0b0010_0000
+	HorzCenter Align = 0b0100_0000
+	Right      Align = 0b1000_0000
 
-	Top          Align = 0b0000_0001 // vertical align
-	Midline      Align = 0b0000_0010 // vertical align
-	VertCenter   Align = 0b0000_1001 // vertical align
-	Baseline     Align = 0b0000_0100 // vertical align
-	LastMidline  Align = 0b0000_1010 // vertical align
-	LastBaseline Align = 0b0000_1100 // vertical align
-	Bottom       Align = 0b0000_1000 // vertical align
+	// Vertical aligns
+	Top          Align = 0b0000_0001 // top of font's ascent
+	Midline      Align = 0b0000_0010 // top of xheight (rarely used)
+	VertCenter   Align = 0b0000_1001 // middle of line height
+	Baseline     Align = 0b0000_0100 // aligned to baseline
+	Bottom       Align = 0b0000_1000 // bottom of font's descent
+	LastMidline  Align = 0b0000_1010 // last Midline (if multiple lines) (rarely used)
+	LastBaseline Align = 0b0000_1100 // last Baseline (if multiple lines) (rarely used)
 
-	Center Align = HorzCenter | VertCenter // full align
+	// Full aligns
+	Center Align = HorzCenter | VertCenter
 	
 	alignVertBits Align = 0b0000_1111 // bit mask
 	alignHorzBits Align = 0b1111_0000 // bit mask
 )
+// Internal note: the fact that the combinations of Bottom | Top,
+// Baseline | Bottom and Midline | Bottom are valid is intentional
+// and I intend to preserve it like that, despite the fact that it
+// is ugly that Top | Baseline and Top | Midline don't result in
+// Baseline and Midline respectively too. Perfect combinations are
+// not possible, so this is almost only a cute detail.
 
-// The renderer's align defines how [Renderer.Draw]() and other operations
+// The renderer's [Align] defines how [Renderer.Draw]() and other operations
 // interpret the coordinates passed to them. For example:
 //  - If the align is set to (etxt.[Top] | etxt.[Left]), coordinates will 
 //    be interpreted as the top-left corner of the box that the text needs
@@ -51,11 +71,11 @@ const (
 //
 // See [this image] for a visual explanation instead.
 //
-// Notice that aligns have separate horizontal and vertical components, so
-// you can use calls like [Renderer.SetAlign](etxt.[Right]) to change only
-// one of the components (the horizontal one in this case).
+// Notice that aligns have a horizontal and a vertical component, so
+// you can use [Renderer.SetAlign](etxt.[Right]) and similar to change only
+// one of the components at a time.
 //
-// By default, the renderer's align is (etxt.[Left] | etxt.[TopBaseline]).
+// By default, the renderer's align is (etxt.[Left] | etxt.[Baseline]).
 //
 // [this image]: https://github.com/tinne26/etxt/blob/main/docs/img/gtxt_aligns.png
 func (self *Renderer) SetAlign(align Align) {
