7954066e804ffbda3f744b4a0195fc5fe1397ef5
tinne26 <tinne26k@gmail.com>
2023-09-04 17:40:01 +0200

    Twines initial example.

--- examples/ebiten/twine/go.mod
+++ examples/ebiten/twine/go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/hajimehoshi/ebiten/v2 v2.5.0
-	github.com/tinne26/etxt v0.0.9-alpha.6.0.20230821225710-e6ea9a4bb2df
+	github.com/tinne26/etxt v0.0.9-alpha.6.0.20230904153132-c90b8cc9f529
 	golang.org/x/image v0.9.0
 )
 
--- examples/ebiten/twine/main.go
+++ examples/ebiten/twine/main.go
@@ -12,6 +12,7 @@ import "github.com/hajimehoshi/ebiten/v2"
 
 import "github.com/tinne26/etxt"
 import "github.com/tinne26/etxt/font"
+import "github.com/tinne26/etxt/fract"
 
 // This example showcases how to use the Twine type instead of
 // a string in order to apply different formats and styles in
@@ -19,12 +20,12 @@ import "github.com/tinne26/etxt/font"
 //
 // The code in this example is very basic and only shows to use the 
 // most basic twine commands. Much more advanced functions are both
-// pre-implemented in etxt and allowed to be implemented by etxt
-// users. See ebiten/twine_all. Another example, bbcode, also shows
+// pre-implemented in etxt and allowed to be implemented through the
+// API. See ebiten/twine_all. Another example, ebiten/bbcode, shows
 // how to create custom types and functions to make twines easier to
 // build when we want to tailor them to some specific usage context.
 // 
-// You can run this example like this:
+// You can run the example like this (you need two fonts):
 //   go run github.com/tinne26/etxt/examples/ebiten/text@latest path/to/regular-font.ttf path/to/bold-font.ttf
 
 type Game struct {
@@ -80,13 +81,16 @@ func main() {
 	renderer.SetSize(16)
 	renderer.SetAlign(etxt.Baseline | etxt.Left)
 	
-	// configure additional bold font
+	// register fonts and functions that we will be using
 	bold := renderer.Complex().RegisterFont(etxt.NextFontIndex, boldFont)
+	spoiler := renderer.Complex().RegisterEffectFunc(etxt.NextEffectKey, (&Spoiler{}).Func)
 
 	// prepare some variables for the twine content
 	var glyphs []sfnt.GlyphIndex
 	for i := 0; i < 16; i++ { glyphs = append(glyphs, sfnt.GlyphIndex(i)) }
-	caramel := color.RGBA{191, 148, 74, 255}
+	caramel := color.RGBA{166, 127, 58, 255} // very dark caramel actually
+	// tea := color.RGBA{221, 232, 185, 255}
+	// carmine := color.RGBA{147, 22, 33, 255}
 
 	// --- create twine content ---
 	// This is the key part of this example. We are only using basic methods,
@@ -96,7 +100,6 @@ func main() {
 	twine := etxt.Weave(
 		bold, "Font name: ", etxt.Pop, fontName, '\n',
 		bold, "Glyphs 0 - 15:", etxt.Pop, " '", glyphs, "'\n\n",
-		bold, "Sample text:", etxt.Pop, '\n',
 		"Twines are like strings, but they allow you to add ", caramel,
 		"formatting\ndirectives", etxt.Pop, " on the text, encode glyphs ",
 		"directly for text shaping,\npass strings as byte ",
@@ -107,18 +110,63 @@ func main() {
 	twine.AddLineBreak()
 	twine.AddLineBreak()
 	twine.Add("There are many ways to encode the content:\n")
-	twine.Weave("- Using Weave(), a very dynamic but somewhat unsafe function", '.')
+	twine.Weave("- Using Weave(), a very dynamic but slightly unsafe function", '.')
 	twine.AddRune('\n')
-	twine.Add("- Chaining ").PushColor(caramel).Add("Twine API").Pop().Add(" methods.").AddLineBreak()
-	twine.Buffer = append(twine.Buffer, []byte("- Going low level and touching what you ")...)
+	twine.Add("- Chaining ").PushColor(caramel).Add("Twine API").Pop().Add(" methods one after another.")
+	twine.AddLineBreak()
+	twine.Buffer = append(twine.Buffer, []byte("- Going low level and touching what you ")...) // *
+	// * This is generally not safe, you would have to know about internal implementation details.
+	//   For example, if we had been adding glyphs right before the append this wouldn't work.
 	twine.PushFont(bold).Add("shouldn't").Pop().AddRune('.')
+
+	twine.Add("\n\n")
+	twine.Add("Manipulating size can be ").ShiftSize(3).Add("dangerous").Pop().Add(", but it's possible with\n")
+	twine.ShiftSize(-3).Add("certain restrictions").Pop().Add(". In particular, line height remains fixed to\n")
+	twine.Add("the initial value unless you explicitly refresh it.\n\n")
+	twine.Add("Not to spoil your life movie, but the main character ")
+	twine.PushEffect(spoiler).Add("trips\nover twines and sustains a traumatic brain injury").Pop().Add(".")
 
 	// run the game
 	ebiten.SetWindowTitle("etxt/examples/ebiten/twine")
 	ebiten.SetWindowSize(640, 480)
+	//etxt.SetAppIcon()
 	err = ebiten.RunGame(&Game{
 		text: renderer,
 		twine: twine,
 	})
 	if err != nil { log.Fatal(err) }
+}
+
+// A simple example of a type that implements a single-use TwineEffectFunc.
+// Notice that this is implemented in a very primitive way. There's no
+// support for multiple uses in a single twine, no reset functionality, etc.
+// 
+// If you want more effect examples, see etxt/twine_builtin_effects.go.
+type Spoiler struct { uncovered, nextUncovered bool }
+func (self *Spoiler) Func(renderer *etxt.Renderer, target etxt.Target, args etxt.TwineEffectArgs) fract.Unit {
+	// prevent misuse
+	args.AssertOnPre(false)
+	
+	// bypass if measuring
+	if args.Measuring() { return 0 }
+
+	// handle each trigger situation
+	switch args.GetTrigger() {
+	case etxt.TwineTriggerPush:
+		self.uncovered = self.nextUncovered
+		self.nextUncovered = false
+	case etxt.TwineTriggerPop, etxt.TwineTriggerLineBreak:
+		if !self.uncovered {
+			args.Rect.Clip(target).Fill(color.RGBA{20, 20, 20, 255})
+		}
+		x, y := ebiten.CursorPosition()
+		inRect := args.Rect.Contains(fract.IntsToPoint(x, y))
+		self.nextUncovered = self.nextUncovered || inRect
+	case etxt.TwineTriggerLineStart:
+		// ignored
+	default:
+		panic("unexpected")
+	}
+
+	return 0
 }
