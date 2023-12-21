9976d74c5ad40ff6e7ac87394194b7e9469751c5
tinne26 <tinne26k@gmail.com>
2023-12-21 17:32:51 +0100

    Rename StaticSizer to CustomVertSizer.

--- sizer/vertical_sizer.go
+++ sizer/vertical_sizer.go
@@ -5,16 +5,16 @@ import . "golang.org/x/image/font/sfnt"
 import "golang.org/x/image/math/fixed"
 import "github.com/tinne26/etxt/fract"
 
-var _ Sizer = (*StaticSizer)(nil)
+var _ Sizer = (*CustomVertSizer)(nil)
 
 // A sizer that ignores the specific vertical metrics provided by
 // the font and instead replaces them with fixed values relative to
 // the font size. This can be used to manually control the line
 // height for a single font or a small set of fonts.
 // 
-// One must call [StaticSizer.NotifyChange]() to properly update
+// One must call [CustomVertSizer.NotifyChange]() to properly update
 // values after changing AscentMult, DescentMult or LineGapMult.
-type StaticSizer struct {
+type CustomVertSizer struct {
 	AscentMult fract.Unit
 	DescentMult fract.Unit
 	LineGapMult fract.Unit
@@ -24,39 +24,39 @@ type StaticSizer struct {
 }
 
 // Satisfies the [Sizer] interface.
-func (self *StaticSizer) Ascent(*Font, *Buffer, fract.Unit) fract.Unit {
+func (self *CustomVertSizer) Ascent(*Font, *Buffer, fract.Unit) fract.Unit {
 	return self.cachedAscent
 }
 
 // Satisfies the [Sizer] interface.
-func (self *StaticSizer) Descent(*Font, *Buffer, fract.Unit) fract.Unit {
+func (self *CustomVertSizer) Descent(*Font, *Buffer, fract.Unit) fract.Unit {
 	return self.cachedDescent
 }
 
 // Satisfies the [Sizer] interface.
-func (self *StaticSizer) LineGap(*Font, *Buffer, fract.Unit) fract.Unit {
+func (self *CustomVertSizer) LineGap(*Font, *Buffer, fract.Unit) fract.Unit {
 	return self.cachedLineHeight - self.cachedAscent - self.cachedDescent
 }
 
 // Satisfies the [Sizer] interface.
-func (self *StaticSizer) LineHeight(*Font, *Buffer, fract.Unit) fract.Unit {
+func (self *CustomVertSizer) LineHeight(*Font, *Buffer, fract.Unit) fract.Unit {
 	return self.cachedLineHeight
 }
 
 // Satisfies the [Sizer] interface.
-func (self *StaticSizer) LineAdvance(*Font, *Buffer, fract.Unit, int) fract.Unit {
+func (self *CustomVertSizer) LineAdvance(*Font, *Buffer, fract.Unit, int) fract.Unit {
 	return self.cachedLineHeight
 }
 
 // Satisfies the [Sizer] interface.
-func (self *StaticSizer) GlyphAdvance(font *Font, buffer *Buffer, size fract.Unit, g GlyphIndex) fract.Unit {
+func (self *CustomVertSizer) GlyphAdvance(font *Font, buffer *Buffer, size fract.Unit, g GlyphIndex) fract.Unit {
 	advance, err := font.GlyphAdvance(buffer, g, fixed.Int26_6(size), hintingNone)
 	if err == nil { return fract.Unit(advance) }
 	panic("font.GlyphAdvance(index = " + strconv.Itoa(int(g)) + ") error: " + err.Error())
 }
 
 // Satisfies the [Sizer] interface.
-func (self *StaticSizer) Kern(font *Font, buffer *Buffer, size fract.Unit, g1, g2 GlyphIndex) fract.Unit {
+func (self *CustomVertSizer) Kern(font *Font, buffer *Buffer, size fract.Unit, g1, g2 GlyphIndex) fract.Unit {
 	kern, err := font.Kern(buffer, g1, g2, fixed.Int26_6(size), hintingNone)
 	if err == nil { return fract.Unit(kern) }
 	if err == ErrNotFound { return 0 }
@@ -68,7 +68,7 @@ func (self *StaticSizer) Kern(font *Font, buffer *Buffer, size fract.Unit, g1, g
 }
 
 // Satisfies the [Sizer] interface.
-func (self *StaticSizer) NotifyChange(_ *Font, _ *Buffer, size fract.Unit) {
+func (self *CustomVertSizer) NotifyChange(_ *Font, _ *Buffer, size fract.Unit) {
 	self.cachedAscent  = size.MulUp(self.AscentMult)
 	self.cachedDescent = size.MulUp(self.DescentMult)
 	self.cachedLineHeight = size.MulUp(self.LineGapMult) + self.cachedAscent + self.cachedDescent
